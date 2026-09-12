153e3656190b2ef2d87f8f83760198e2d45b30f9
KayLehnet <243999368+Kalirusi5511@users.noreply.github.com>
2026-09-12 17:11:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ process.on('uncaughtException', (error) => {
 });
 
 // ===== KONFIG-WERTE =====
-const OWNER_ID = '926178878882467930'; // KochSalzChemiker – HIER deine aktuelle ID eintragen!
+const OWNER_ID = '1358450873646321696'; // KochSalzChemiker – HIER deine aktuelle ID eintragen!
 const LOG_CHANNEL_ID = config.adminLogChannelId || config.logChannelId;
 
 // ===== PERSISTENTE LISTEN =====
